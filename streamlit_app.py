c422f02e158e4b7b642c5e605fc2c0ea430c8250
Akshit Kotian <51737503+akshit61@users.noreply.github.com>
2022-12-29 17:07:20 +0530

    fix error

--- streamlit_app.py
+++ streamlit_app.py
@@ -63,7 +63,7 @@ def insert_from_snowflake(new_fruit):
 
 add_my_fruit = streamlit.text_input('What fruit would you like to add?')
                                     
-if streamlit.buttor('Add a fruit to the list'):
+if streamlit.button('Add a fruit to the list'):
     my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
     back_from_function = insert_from_snowflake(add_my_fruit)
     streamlit.text(back_from_function)
